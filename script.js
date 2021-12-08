f1e99cbca16916466a4165fb23b089981f39c696
smokysalmon <hlee2501@gmail.com>
2021-12-07 17:19:16 -0800

    removed unneeded class

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 makeBoard(16);
+let button = document.querySelector("#clear_button");
+button.addEventListener("click", event => {
+    clear();
+});
 
 function addRow(n) {
     // n is number of squares in row
@@ -8,6 +12,9 @@ function addRow(n) {
     for (let k = 0; k < n; k++) {
         let square = document.createElement("div");
         square.classList.toggle("square");
+        square.addEventListener("mouseover", event => {
+            square.style.backgroundColor = "black";
+        });
         row.appendChild(square);
     }
     container.appendChild(row);
@@ -17,4 +24,18 @@ function makeBoard(n) {
     for (let k = 0; k < n; k++) {
         addRow(n);
     }
+}
+
+function clear() {
+    let nString = prompt("Enter the amount of rows you want.", "16");
+    let n = 16;
+    if (!isNaN(nString) && !(n > 100 || n < 1)) {
+        n = parseInt(nString);
+    }
+    let container = document.querySelector("#container");
+    let rowList = document.querySelectorAll(".row");
+    rowList.forEach(row => {
+        container.removeChild(row);
+    });
+    makeBoard(n);
 }
